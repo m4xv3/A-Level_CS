1739bdb7cd18859076677a1e34bad5331cd45b2c
Maximilian Eventov <292918321+m4xv3@users.noreply.github.com>
2026-09-16 09:32:20 +0000

    Refactor character classes and implement player selection

--- fighting_game.py
+++ fighting_game.py
@@ -1,3 +1,44 @@
 # Make a character class with 3 subclasses - Warrior, Elfin, Wizord. Warrior: Health, axe, Higher Strength. Elf: Health, speed. Wizard: Health, Magic. 
 # Two characters fight, attacks generate (random) damage on health, when health reaches 0, winner is declared.
-# Print status of characters, attack value (damage), 
+# Print status of characters, attack value (damage)
+import random
+class Character():
+    def __init__(self, name, weapon, damage, health = 100, dodge = random.randint(0, 1)):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.dodge = dodge
+        self.weapon = weapon
+
+    def attack():
+        opp.health -= random.randint(1,10)
+
+class Wizard(Character):
+    def __init__(self, "Shaft", pos_expression = "YEHAW", neg_expression = "YEOW"):
+        super().__init__("Wizard", "Shaft", random.randint(20, 40), health, dodge)
+        self.pos_expression = pos_expression
+        self.neg_expression = neg_expression
+
+class Elf(Character):
+    def __init__(self, pos_expression = "YEHAW", neg_expression = "YEOW"):
+        super().__init__("Elf", "Glock", random.randint(5,15), health, dodge)
+        self.pos_expression = pos_expression
+        self.neg_expression = neg_expression
+
+class Warrior(Character):
+    def __init__(self, pos_expression = "YEHAW", neg_expression = "YEOW"):
+        super().__init__("Warrior", "Cutlass", random.randint(30, 55), health, dodge)
+        self.pos_expression = pos_expression
+        self.neg_expression = neg_expression
+
+Brawler1 = input("Choose your first player")
+Brawler2 = input("Choose your second player")
+characters= [
+    "Wizard": Wizard()
+    "Elf" : Elf()
+    "Warrior" : Warrior()
+]
+charcter[Brawler1]()
+        
+
+        
